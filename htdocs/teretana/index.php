a06b70e7ada6ca4a35180226394dabd516acdc3b
AminGule <156127854+AminGule@users.noreply.github.com>
2024-03-26 15:38:57 +0100

    Add files via upload

---
+++ htdocs/teretana/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <title>Admin Login</title>
+</head>
+<body>
+    <form action = "" method = "POST">
+        Username: <input type = "text" name = "username"><br>
+        Password: <input type = "password" name = "password"><br>
+        <input type = "submit" value = "Login" >
+    </form>
+</body>
+</html>
